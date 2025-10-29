c31c847742a346a34ffd317662aa3a1f4f35ebec
Katarzyna Kabala <katarzyna_kabala@o2.pl>
2025-10-29 18:42:58 +0100

    Implement cypress. Login flow test

--- src/components/LabelField/LabelField.js
+++ src/components/LabelField/LabelField.js
@@ -1,13 +1,15 @@
 import s from "./LabelField.module.css";
-export const LabelField = ({ type, title, state, setState }) => {
+export const LabelField = ({ type, title = "", state, setState }) => {
+  const labelText = title ? title.charAt(0).toUpperCase() + title.slice(1) : "";
   return (
     <label className={s.label}>
-      <p>{title.charAt(0).toUpperCase() + title.slice(1)}</p>
+      <p>{labelText}</p>
       <input
         className={s.input}
         type={type}
         value={state}
         minLength={4}
+        data-cy={title ? `input-${title}` : undefined}
         onChange={(e) => setState(e.target.value)}
       ></input>
     </label>
